98259f87b0f0653c23ff34ef031b2c6c07e2f623
GuiQuad06 <guillaume.cuadrado@gmail.com>
2024-08-12 09:42:42 +0200

    Implement Enigme 2

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def process(a, b, ope):
+    if ope == '+':
+        return a + b
+    elif ope == '-':
+        return a - b
+    elif ope == '*':
+        return a * b
+    elif ope == '/':
+        return a / b
+    else:
+        print('No supported operator')
+
+
+def operation(data):
+    stack = []
+    for char in data:
+        if char.isnumeric():
+            stack.append(int(char))
+        elif char in ['+', '-', '*', '/']:
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(process(a, b, char))
+        else:
+            raise ValueError(f'The element {char} is not valid...')
+    return stack[0]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    input_str = input('Veuillez rentrer une chaine representant un calcul:')
+
+    sequence = input_str.split(' ')
+    print(operation(sequence))
