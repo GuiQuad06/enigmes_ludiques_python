1ca5fccd6f0fee6e6927d9cb51ad374f0b63c59e
GuiQuad06 <guillaume.cuadrado@gmail.com>
2024-08-13 00:27:49 +0200

    Implement the hash table in json file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# This is a sample Python script.
+import json
+import hashlib
+import itertools
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+dico = {}
 
 
 def print_hi(name):
@@ -9,8 +11,46 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def generate_pwd():
+    global dico
+    # Define the set of characters (lowercase letters)
+    characters = 'abcdefghijklmnopqrstuvwxyz'
+    #characters = 'abc'
+    # Generate all possible 5-character combinations
+    passwords = itertools.product(characters, repeat=5)
+
+    # Iterate over the passwords
+    for password in passwords:
+        h = hashlib.sha256(bytes(''.join(password), 'utf-8'))
+        dico[h.hexdigest()] = ''.join(password)
+
+
+def write_in_file(file_name):
+    global dico
+    with open(file_name, mode='w', encoding='utf-8') as fd:
+        json.dump(dico, fd)
+
+def read_from_file(file_name):
+    with open('hash.json', mode='r', encoding='utf-8') as fd:
+        received_dico = json.load(fd)
+
+    print(received_dico.get('9e69e7e29351ad837503c44a5971edebc9b7e6d8601c89c284b1b59bf37afa80'))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    start = time.time()
+
+    generate_pwd()
+    write_in_file('hash.json')
+
+    stop = time.time()
+    print(f'program ended in {stop - start} seconds')
+
+    start = time.time()
+    read_from_file('hash.json')
+
+    stop = time.time()
+    print(f'password found in {stop - start} seconds')
