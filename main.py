08a3abd310aea2507a02b48479e301dc216ec8f7
Guillaume Cuadrado (GUCD) (Consultant) <gucd@oticonmedical.com>
2024-08-31 22:35:18 +0200

    Resolve Puzzle

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-# This is a sample Python script.
+cartes = [
+    [1, 4, 6, 9, 12, 14, 17, 19, 22, 25, 27, 30, 33],
+    [2, 7, 10, 15, 20, 23, 28, 31],
+    [3, 4, 11, 12, 16, 17, 24, 25, 32, 33],
+    [5, 6, 7, 18, 19, 20, 26, 27, 28],
+    [8, 9, 10, 11, 12, 29, 30, 31, 32, 33],
+    [13, 14, 15, 16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+value_carte = 0
+
+def fibo(n):
+    if n <= 2:
+        return n
+    else:
+        return fibo(n - 1) + fibo(n - 2)
 
 
 def print_hi(name):
@@ -12,5 +24,15 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    choosen_num = int(input('veuillez rentrer un numero entre 0 et 33'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for index, carte in enumerate(cartes):
+        print(f'Voici les nombres de la carte {index}')
+        for num in carte:
+            print(num)
+        isCartePresente = input('la carte choisie figure t elle dans la collection ? O/n')
+        if isCartePresente.lower() == 'o':
+            value_carte += fibo(index + 1)
+
+    print('ok') if value_carte == choosen_num else print('nok')
+
