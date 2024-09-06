3b0dc45aadb23908291e0bf858cc79e0bec1086f
Guillaume Cuadrado (GUCD) (Consultant) <gucd@oticonmedical.com>
2024-09-06 14:05:48 +0200

    Hack the SECRET

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from huffman_algo import compresser_bits
 from huffman_secret import SECRET
+import string
 
 
 def print_hi(name):
@@ -9,11 +10,26 @@ def print_hi(name):
 
 def oracle(msg):
     msg_compresse = compresser_bits(SECRET + msg)
-    #return len(msg_compresse)
+    return len(msg_compresse)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    char = []
+    dico = {}
     print_hi('PyCharm')
-    message = input("Entrez un message:")
-    print(oracle(message))
+    print('Craquage du Secret !!!')
+
+    # When we concatenate a new character after the SECRET, the len returned by the oracle
+    # (the length of a compressed message) can take 2 values, and the value closer to
+    # the original oracle message, represent a character already contained in the secret
+    # Step 1: Process the two oracle values and identify the minimum one
+    for c in string.ascii_lowercase:
+        dico[c] = oracle(c)
+    contained_char_oracle = min(dico.values())
+
+    # Step 2: Generate a list with the characters contained in the secret message
+    for key, value in dico.items():
+        char.append(key) if value == contained_char_oracle else None
+
+    print(char)
