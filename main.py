3cea41a79ecb9b98ae9646f35aeb8ce667870b2b
Guillaume Cuadrado <gcuadrado@cochlear.com>
2025-08-13 17:07:21 +0200

    2025 updates

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import hashlib
 import itertools
@@ -5,17 +6,19 @@ import time
 
 dico = {}
 
+USAGE = "python main.py [-f]"
+DESCRIPTION = "Enigme 4 Hash table, -f option is to provide full letters choice"
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def generate_pwd():
+def generate_pwd(full):
     global dico
     # Define the set of characters (lowercase letters)
-    characters = 'abcdefghijklmnopqrstuvwxyz'
-    #characters = 'abc'
+    characters = 'abcdefghijklmnopqrstuvwxyz' if full else 'abc'
     # Generate all possible 5-character combinations
     passwords = itertools.product(characters, repeat=5)
 
@@ -39,11 +42,15 @@ def read_from_file(file_name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION)
+    parser.add_argument("-f", dest="full_alpha", action='store_true', default=False)
+    arg = parser.parse_args()
+
     print_hi('PyCharm')
 
     start = time.time()
 
-    generate_pwd()
+    generate_pwd(arg.full_alpha)
     write_in_file('hash.json')
 
     stop = time.time()
