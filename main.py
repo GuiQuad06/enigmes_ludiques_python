f86d7e290e8abbc206e6b29af617e5663af28f97
GuiQuad06 <guillaume.cuadrado@gmail.com>
2025-09-03 22:58:07 +0200

    Implement the game base, algo bot not done yet

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from nim import Nim
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    my_nim = Nim(n = 21, p = 3)
+    player1_name = input('Please enter your name, player 1: ')
+    player2_name = 'bot'
+    players = (player1_name, player2_name)
+    turn = 0
+
+    while my_nim.cpt > 1:
+        print(my_nim)
+        if players[turn] == 'bot':
+            print('Bot is playing...')
+            my_nim.bot_play()
+        else:
+            print(f'Please proceed {players[turn]}')
+            play = int(input('How many ?'))
+            my_nim.play(play)
+        turn = (turn + 1) % 2
+
+    print(f'You lost {players[turn]}')
---
+++ nim.py
@@ -0,0 +1,29 @@
+class Nim:
+    def __init__(self, n, p, bot=False):
+        self.n = n
+        self.p = p
+        self.cpt = n
+        self.bot = bot
+        self.possibilities = self.process_p()
+
+    def process_p(self):
+        tmp = []
+        for i in range (self.p):
+            tmp.append(i + 1)
+        return tmp
+
+    def __repr__(self):
+        res = ''
+        for _ in range(self.cpt):
+            res += '| '
+        return res
+
+    def play(self, m):
+        if m not in self.possibilities:
+            raise Exception(f'Value should be in this range :{self.possibilities}')
+        if self.cpt:
+            self.cpt -= m
+
+    def bot_play(self):
+        if self.cpt:
+            self.cpt -= 1
