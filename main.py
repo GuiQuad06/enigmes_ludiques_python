bfd5d518394800b7553fd4279a3aeb01a74b977e
GuiQuad06 <guillaume.cuadrado@gmail.com>
2025-09-03 22:16:55 +0200

    Implement puzzle

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import string
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ALPHA = list(string.ascii_lowercase)
 
+def from_letter_to_index(char):
+    for i, letter in enumerate(ALPHA):
+        if char.lower() == letter:
+            return i
+    return None
+
+def from_index_to_letter(index):
+    for i, letter in enumerate(ALPHA):
+        if index == i:
+            return letter
+    return None
+
+def process_key(cipher, msg):
+    _key = []
+    for i in range(len(cipher)):
+        a = from_letter_to_index(cipher[i])
+        b = from_letter_to_index(msg[i])
+        _key.append(from_index_to_letter((a - b) % 26))
+    return _key
+
+def decrypt(cipher, key, file_path):
+    msg = []
+    shift_key = 0
+    for i in range(len(cipher)):
+        if cipher[i] not in list(string.ascii_lowercase) and cipher[i] not in list(string.ascii_uppercase):
+            msg.append(cipher[i])
+            shift_key += 1
+        else:
+            a = from_letter_to_index(cipher[i])
+            b = from_letter_to_index(key[i - shift_key])
+            msg.append(from_index_to_letter((a - b) % 26))
+    with open(file_path, 'w') as f:
+        f.write(''.join(msg))
+
+def parse_input(file_path):
+    with open(file_path, 'r') as f:
+        cipher = f.read()
+    return cipher
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    key = []
+    secretMessage = parse_input('inputs//enigme9.txt')
+    shortKey = 'python'
+    for i in range( len(secretMessage)):
+        key.append(shortKey[i % (len(shortKey))])
+
+    decrypt(secretMessage, key, 'output.txt')
