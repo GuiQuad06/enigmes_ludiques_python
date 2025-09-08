db64467e470ad130e46f8944757329cf39c122f5
GuiQuad06 <guillaume.cuadrado@gmail.com>
2025-09-08 20:56:18 +0200

    Implement puzzle

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+TRUCHET_VALUES = [0, 1, 2, 3]
+CONVERSION_MAP = [0, 0, 1, 1]
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def parse_input(path):
+    with open(path) as f:
+        message = f.read().splitlines()
+        # The map can cast str to int
+        message = [ list(map(int, line.split(' '))) for line in message ]
+    return message
 
-# Press the green button in the gutter to run the script.
+def convert_2_ascii(byte):
+    res = 0
+    for i in range(7, -1, -1):
+        res += pow(2, i) * byte[7 - i]
+    return res
+
+def process(data):
+    res = ''
+    for character in data:
+        ascii = []
+        for elt in character:
+            if elt not in TRUCHET_VALUES:
+                continue
+            else:
+                ascii.append(CONVERSION_MAP[elt])
+        res += chr(convert_2_ascii(ascii))
+    return res
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    toto = parse_input('inputs//enigme11.txt')
+    print(process(toto))
